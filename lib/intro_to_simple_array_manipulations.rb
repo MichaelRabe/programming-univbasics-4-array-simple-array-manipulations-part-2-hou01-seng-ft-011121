cb75f5ba8868dcb36385e000b287bdfc2c3135fa
Michael Rabe <m.trabe96@gmail.com>
2020-12-22 01:01:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat (array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element= "Python")
+def using_insert(array, another_language)
   array.insert(element.to_i)
 end
 
